d937653a2a19a8ea1cecce73133e3e0c65845262
corinthias <89884709+corinthias2025@users.noreply.github.com>
2025-07-18 15:58:04 -0300

    Delete api directory

--- api/baixar.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function handler(req, res) {
-  res.redirect(302, 'https://onedrive.live.com/?ls=true&cid=D82992BFEEE0874A&id=D82992BFEEE0874A%21s80a236759e494466a94e483cb279f774&parId=root&o=OneUp');
-}
